c115a6870bf8cee939313752a1606048cf16a361
Daniel W. Hieber <dwhieb@gmail.com>
2022-02-20 02:34:56 -0700

    NEW: use curly quotes (#19)
    closes #19

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,21 @@
-module.exports = function(eleventyConfig) {
+const md = require(`markdown-it`);
 
-  eleventyConfig.addPassthroughCopy(`bibliography/bibliography.js`);
-  eleventyConfig.addPassthroughCopy(`favicon.ico`);
-  eleventyConfig.addPassthroughCopy(`images`);
+const mdOptions = {
+  html       : true,
+  quotes     : `“”‘’`,
+  typographer: true,
+};
+
+const mdParser = md(mdOptions);
+
+module.exports = function(config) {
+
+  config.addPassthroughCopy(`bibliography/bibliography.js`);
+  config.addPassthroughCopy(`favicon.ico`);
+  config.addPassthroughCopy(`images`);
+  config.addPassthroughCopy({ 'layouts/main.css': `index.css` });
+
+  config.setLibrary(`md`, mdParser);
 
   return {
     dir: {
--- home/home.md
+++ home/home.md
@@ -1,5 +1,6 @@
 ---
-title: Home
+permalink: /
+title:     Home
 ---
 # Algonquian Database Project
 
